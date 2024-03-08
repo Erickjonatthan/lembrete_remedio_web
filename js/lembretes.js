3ebe1d86c89b54d804a4a1af1666dc4ba8370184
Erick Jonathan <zxjonathanxz22@gmail.com>
2024-03-08 13:08:21 -0300

    Refactor lembretes.js and lembretes.html

--- js/lembretes.js
+++ js/lembretes.js
@@ -26,3 +26,40 @@ fetch(`http://localhost:8080/cadastro/${userId}`, { headers })
         console.error('Error:', error);
         alert(error.message);
     });
+
+// Função para obter dados do lembrete
+function getReminderData(pageNumber) {
+    fetch(`http://localhost:8080/lembretes?page=${pageNumber}`, { headers })
+        .then(response => response.json())
+        .then(data => {
+            if (data.content.length === 0) {
+                showNoRemindersMessage();
+            } else {
+                data.content.forEach(updateReminderData); // Atualiza a página para cada lembrete
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// Função para atualizar os dados do lembrete na página
+function updateReminderData(reminder) {
+    const reminderTemplate = document.querySelector('.reminder-template');
+    const reminderElement = reminderTemplate.cloneNode(true);
+    reminderElement.style.display = 'block';
+    reminderElement.querySelector('.start-date').textContent = reminder.dataInicio;
+    reminderElement.querySelector('.medication-name').textContent = reminder.nomeMedicamento;
+    reminderElement.querySelector('.time').textContent = reminder.horario;
+    reminderElement.querySelector('.dosage').textContent = reminder.dosagem;
+    reminderElement.querySelector('.hour-interval').textContent = reminder.intervaloHora;
+    document.querySelector('.container').appendChild(reminderElement);
+}
+
+// Função para mostrar uma mensagem quando não há lembretes
+function showNoRemindersMessage() {
+    const messageElement = document.createElement('p');
+    messageElement.textContent = 'Você não tem nenhum lembrete.';
+    document.querySelector('.container').appendChild(messageElement);
+}
+
+// Chamar a função quando a página carregar
+window.onload = () => getReminderData(0); // Começa na página 0
